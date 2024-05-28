983808195f791ef14554575dd108321d3e7a7f80
choivember <pocketmongyo@gmail.com>
2024-05-28 15:16:09 +0900

    back-end 기초 프로젝트 생성

---
+++ src/test/java/com/example/swheatsbackend/SWheatsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.swheatsbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SWheatsBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
